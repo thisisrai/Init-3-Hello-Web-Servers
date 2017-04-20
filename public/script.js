271c2a054171d5c7131cffb6780a1e1dc229071d
Rai Lee <thisisrailee@gmail.com>
2017-04-19 17:15:35 -0700

    Finished stage3 specs

--- public/script.js
+++ public/script.js
@@ -1,5 +1,42 @@
-function myFunction() {
-  console.log(document.getElementById("text-input").value);
-  var x = document.getElementById("text-input").value;
-  document.getElementById("preview-render-box").innerHTML = x;
+let updatePreview = () => {
+  let editorInput = document.getElementById('text-input').value
+  let markdown = marked(editorInput)
+  document.getElementById('preview-render-box').innerHTML = markdown
+}
+
+let saveFile = () => {
+  let markdownData = document.getElementById('text-input').value
+  fetch('/save', {
+    method: 'post',
+    headers: {
+      'Content-Type': 'text/plain'
+    },
+    body: markdownData
+  })
+  .then( response => {
+    console.log('we did it!', response);
+  })
+}
+
+
+let load = () => {
+  fetch('/load', {
+    method: 'get',
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  })
+  .then((response) => {
+    return response.text()
+  })
+  .then((response) => {
+    document.getElementById('text-input').value = response
+    updatePreview()
+  }).catch(console.log)
+}
+
+window.onload = () => {
+  document.getElementById('text-input').addEventListener('input', updatePreview)
+  document.getElementById('button-save').addEventListener('click', saveFile)
+  load()
 }
