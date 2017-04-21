1326e173f92c50ce105de6f778012f4fc84277b7
smithrm941 <smithrm941@gmail.com>
2017-04-21 11:16:19 -0700

    complete most stage 4 specs

--- app.js
+++ app.js
@@ -5,15 +5,10 @@ const bodyParser = require('body-parser')
 const fs = require('fs')
 const app = express()
 
-
-// *****For public view files e.i. css****
 app.set('view engine', 'pug')
 app.set('views', 'views')
 app.use(bodyParser.text())
 app.use(express.static(path.join(__dirname, 'public')))
-
-// For save button
-
 
 app.get('/', function(request, response){
   fs.readdir('data', function(err, files) {
@@ -32,16 +27,13 @@ app.post('/save', function(request, response){
   response.send('we posted!')
 })
 
-//new file attempt:
 app.post('/newfile', function(request, response){
   console.log('we got', request.body);
   let writeStream = fs.createWriteStream('data/' + request.body + '.md')
-  writeStream.write(request.body)
   writeStream.end()
   response.send('we posted!')
 })
 
-// Trial stuff
 app.get('/:newfile', function(request,response){
   let str = ''
   let readStream = fs.createReadStream('data/'+request.params.newfile)
@@ -52,23 +44,10 @@ app.get('/:newfile', function(request,response){
       response.render('index', {files: files, data: str})
     })
   })
-
-
 })
-
-// app.get('/load', function(request,response){
-//   let str = ''
-//   let readStream = fs.createReadStream('data/markdown.md')
-//   readStream.on('data', function(chunk){
-//     str += chunk
-//   }).on('end', function(){
-//     response.send(str)
-//   })
-// })
 
 app.get('*', function(request, response){
   console.log(request.url)
 })
-
 
 app.listen(3000)
